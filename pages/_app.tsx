1487f278e29fffa30fac35e961d313591ab63dc3
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-08-03 20:10:24 +0700

    feat: complete init🎉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "$styles/globals.scss";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Colors Generator</title>
+        <meta name="description" content="Just a colors generator" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import("tailwindcss").Config} */
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css,scss}",
+  ],
+};
+
+module.exports = config;
